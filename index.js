cd74e1722e6648325a9646842ef8ae3060de60f9
Alexandre Prediam <aprediam@gmail.com>
2022-03-11 17:26:50 +0100

    enregistrement global

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
- 
+const dice = document.querySelector(".container-dice");
+const btnNew = document.querySelector(".btn-new-game");
+const btnRoll = document.querySelector(".btn-roll-dice");
+const btnHold = document.querySelector(".btn-hold");
+const results1 = document.querySelector(".results-1");
+const results2 = document.querySelector(".results-2");
+const score1 = document.querySelector(".score-1");
+const score2 = document.querySelector(".score-2");
